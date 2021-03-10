061ff4ccb46119b2755d2716fd7d2052910d2d85
nilamma <nilamma@cybage.com>
2021-03-10 17:02:44 +0530

    Adding code for test case CRMT35

--- src/test/java/crmframework/crmAutomation/ContactPageTest.java
+++ src/test/java/crmframework/crmAutomation/ContactPageTest.java
@@ -1,6 +1,9 @@
 package crmframework.crmAutomation;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -41,7 +44,7 @@ public class ContactPageTest extends base{
 	Actions act;
 	CRMContactPage cp;
 	CRMAddMarketingRelationshipOwner amro;
-	
+
 	@BeforeTest
 	public void initialize() throws IOException
 	{
@@ -188,7 +191,7 @@ public class ContactPageTest extends base{
 		hp.getSearchContactField().click();
 		hp.getSearchContactField().sendKeys(newcontactname);
 		hp.getstartsearch().click();
-	
+
 		WebElement validateContactName = driver.findElement(By.xpath("//label[contains(text(),'"+newcontactname+"')]"));
 		validateContactName.click();
 		ap.getAccNaviagteBtn().click();
@@ -202,15 +205,15 @@ public class ContactPageTest extends base{
 		outofbusiness = cp.getContactStatusOutofBusiness().getText();
 		System.out.println("Contact Status: " + outofbusiness);
 		outofbusinessstatus.click();
-		
+
 		//Click on 'Deactivate button of confirmation pop-up
 		cp.getDeactivateOkBtn().click();
-		
+
 		//Verify that Contact is deactivated and selected contact status reason is displayed at the right side of the header.
 		WebElement statusreasonforinactivecontactinheader = cp.getContactStatusResonForInactiveAcc();
 		System.out.println("Account Status Reason: " + (statusreasonforinactivecontactinheader.getText()));
 		Assert.assertTrue(statusreasonforinactivecontactinheader.getText().contains(outofbusiness));
-		
+
 		//Verify that Top ribbon 'Deactivate' option changes to 'Activate'
 		Assert.assertTrue(ap.getActivateBtn().isDisplayed());
 
@@ -244,7 +247,7 @@ public class ContactPageTest extends base{
 			System.out.println(ex.getMessage());
 		}
 	}
-	
+
 	@Test(priority=4)
 	public void TS004_VerifyMarketingRelationshipOwnerContactTest() throws InterruptedException
 	{
@@ -255,20 +258,20 @@ public class ContactPageTest extends base{
 		hp = new CRMHomePage(driver);
 		ap = new CRMAccountsPage(driver);
 		cp = new CRMContactPage(driver);
-		
+
 
 		//Open Contacts page and open existing contact
 		hp.getContactsTab().click();
 		cp.getselectexistingcontact().click();
 		cp.getscrollrightongrid().sendKeys(Keys.ARROW_RIGHT);
 		cp.getopenexistingcontact().click();
-		
+
 		amro = new CRMAddMarketingRelationshipOwner(driver);
-		
+
 		//Click Arrow for Marketing Relationship Owner
-		
+
 		amro.gethdbtn().click();
-		
+
 		//Click on Marketing Relationship Owner field search icon  to select a user from lookup
 		amro.getmarlookupsearch().click();
 
@@ -284,20 +287,124 @@ public class ContactPageTest extends base{
 		//Scroll down the page till Address field
 		act = new Actions(driver);
 		act.moveToElement(ap.getAddress()).perform();
-		
+
 		//Verify Marketing Relationship Owner lookup value in Account Information section in the Summary tab
 		WebElement verifyOwner = amro.getconmarverifydd();
 		Assert.assertTrue(verifyOwner.getText().contains(ownertxt));
 		System.out.println("Marketing Relationship Owner is added successfully");
-		
+
 		//Navigate back to Active accounts list
 		ap.getPageBackBtn().click();
 	}
 
-//	@AfterTest
-//	public void closeDriver()
-//	{
-//		driver.close();
-//	}
+	//Manual Fail_Caught By Automation	
+	@Test(priority=5)
+	public void TS005_VerifyJobFunctionFieldTest() throws InterruptedException
+	{
+		//The purpose of this test case to verify:-
+		//CRM-T35- Job Function drop-down field is visible on Contact form only if Type equal to 'Media' is selected
+
+		hp = new CRMHomePage(driver);
+		ap = new CRMAccountsPage(driver);
+		cp = new CRMContactPage(driver);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS) ;
+
+		//Click on Contacts Tab at left menu
+		hp.getContactsTab().click();
+
+		//Click on 'New' button to open new Contact Form
+		cp.getCreateNewContactBtn().click();
+
+		//Enter First Name & Last name
+		cp.getfirstname().click();
+		String ContactFirstName = "QA"+ genData.generateRandomString(3);
+		cp.getfirstname().click();
+		cp.getfirstname().sendKeys(ContactFirstName);
+
+		//Select contact Type as 'Media' in Contact information section
+		cp.getContactTypetxtbx().click();
+		cp.getContactTypeExpandbtn().click();
+		cp.getContactTypeMedia().click();
+		cp.getContactFirstNameLabel().click();
+
+		//Verify that 'Job Function' field should now be displayed under Contact information section on contact form
+		List<WebElement> jobfunctionlabel = cp.getJobFunctionFieldLabel();
+		Assert.assertTrue(jobfunctionlabel.size()!= 0);
+
+		//Click on 'Job Function' drop down
+		cp.getJobFunctionFieldTxtBox().click();
+		cp.getJobFunctionFieldExpandBtn().click();
+		Thread.sleep(3000);
+
+		//Verify that Default value for Job Function should be empty
+		Assert.assertTrue(cp.getJobFunctionFieldTxtBox().getText().isEmpty());
+
+		// Verify that All the values should be in alphabetically ascending order
+		List<String> tempList = new ArrayList();
+		List<String> originalList = new ArrayList();
+		List<WebElement> jobfunctionvalueslist = cp.getJobFunctionvValuesList();
+
+		for (int i=0; i<jobfunctionvalueslist.size();i++) 
+		{
+			String listitem = jobfunctionvalueslist.get(i).getText();
+			originalList.add(listitem);
+			tempList.add(listitem);
+		}
+		System.out.println("Job Function values before Sorting: " + tempList);
+		Collections.sort(tempList);
+		System.out.println("Job Function values after Sorting: " + tempList);
+
+		//Manual Fail for Alphabetical order
+		//Assert.assertEquals(tempList, originalList, "List is not in alphabetical sorting order");
+
+		//Select a value in Job Function field
+		cp.getJobFunctionValue().click();
+		cp.getScrollTextOnContactForm().click();
+
+		//Save the form by filling other mandatory fields
+		//Enter Account Name
+		cp.getContactAccountNameTxtbx().click();
+		cp.getSearchRecordsBtn().click();
+		cp.getAccountNameTitle().click();
+
+		WebElement scrollText = cp.getScrollTextOnContactForm();	
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		jse.executeScript("arguments[0].scrollIntoView(true);",scrollText);
+		Thread.sleep(3000);
+
+		cp.getemail().click();
+		cp.getemail().sendKeys(genData.generateEmail(15));
+		cp.getmobile().sendKeys(genData.generateRandomNumber(10));
+		cp.getMobilePhoneLabel().click();
+		cp.getsavecontact().click();
+		cp.getContactFormRefreshBtn().click();
+
+		//Scroll till email and business phone fields
+		WebElement enteranotelabel = ap.getEnteraNoteLabel();	
+		JavascriptExecutor jse1 = (JavascriptExecutor)driver;
+		jse1.executeScript("arguments[0].scrollIntoView(true);",enteranotelabel);
+		Thread.sleep(5000);
+
+		//Update the contact Type as 'Buyer' 
+		cp.getContactTypeSelectedValueTxtbx().click();
+		cp.getRemoveContactTypeMediaBtn().click();
+		cp.getContactTypeExpandbtn().click();
+		cp.getContactTypeBuyer().click();
+		cp.getsavecontact().click();
+		cp.getContactFormRefreshBtn().click();
+
+		//Verify that Job Function field should be disappeared from the new contact form
+		List<WebElement> jobfunctionlabel1 = cp.getJobFunctionFieldLabel();
+		Assert.assertFalse(jobfunctionlabel1.size()!= 0);
+
+		//Navigate back to Active Contacts list
+		ap.getPageBackBtn().click();
+	}
+
+	//	@AfterTest
+	//	public void closeDriver()
+	//	{
+	//		driver.close();
+	//	}
 
 }
