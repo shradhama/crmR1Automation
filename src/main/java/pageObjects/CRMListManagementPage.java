dabc4fdb94bfc258fe80208ab9bbe9c428e37632
shradhama <shradhama@cybage.com>
2021-04-12 19:45:11 +0530

    Lists Push

--- src/main/java/pageObjects/CRMListManagementPage.java
+++ src/main/java/pageObjects/CRMListManagementPage.java
@@ -15,7 +15,12 @@ public class CRMListManagementPage {
 	By listsexportdropdown= By.xpath("//button[@data-id = 'xxc_list|NoRelationship|HomePageGrid|Mscrm.HomepageGrid.xxc_list.ExportToExcel.Menu$splitButtonId']");//Locator for export to excel dropdown
 	By selectcheckbox1 = By.xpath("//input[@id='id-3a9ef5c2-02f2-49fc-b79e-5bb37b19ac68-1-entitySelector_id3-entitySelector_id.fieldControl-selectAllCheckBoxElementKeyxxc_listcreatedby']");//Locator for checkbox
 	By selectcheckbox2 = By.xpath("//input[@id='id-3a9ef5c2-02f2-49fc-b79e-5bb37b19ac68-1-entitySelector_id3-entitySelector_id.fieldControl-selectAllCheckBoxElementKeyxxc_listcreatedon']");//Locator for checkbox
-	
+	//By selectview = By.xpath("li[contain (text(),'Co-Op List Query for Un-contacted Accounts')");//Locator for view in drop down for 'Co-Op List Query for Un-contacted Accounts'
+	By pagegrid = By.xpath("//div[@aria-label = 'Editable Grid']");//Locator for entity grid
+	By pinuncontacted = By.xpath("//div[@aria-label = 'Pin Co-Op List Query for Un-contacted Accounts']");//Locator for pin button for 'Co-Op List Query for Un-contacted Accounts'
+	By unpin = By.xpath("//div[@title = 'Default view for this list']");//Locator for unpining default view
+	By listmembersremoved = By.xpath("//div[@aria-label = 'List Members Removed']");//Locator for LIst Members Removed view
+	By openlist = By.xpath("//div[@data-id = 'xxc_list.fieldControl-LookupResultsDropdown_xxc_list_selected_tag_text']");//Locator for List at List Member page
 	
 	public CRMListManagementPage(WebDriver driver) {
 
@@ -40,5 +45,35 @@ public class CRMListManagementPage {
 		wait = new WebDriverWait (driver,15);
 		wait.until(ExpectedConditions.elementToBeClickable(selectcheckbox2));
 		return driver.findElement(selectcheckbox2);
+	}
+	public WebElement getpagegrid() {
+
+		wait = new WebDriverWait (driver,15);
+		wait.until(ExpectedConditions.elementToBeClickable(pagegrid));
+		return driver.findElement(pagegrid);
+	}
+	public WebElement getpinuncontacted() {
+
+		wait = new WebDriverWait (driver,15);
+		wait.until(ExpectedConditions.elementToBeClickable(pinuncontacted));
+		return driver.findElement(pinuncontacted);
+	}
+	public WebElement getlistmembersremoved() {
+
+		wait = new WebDriverWait (driver,15);
+		wait.until(ExpectedConditions.elementToBeClickable(listmembersremoved));
+		return driver.findElement(listmembersremoved);
+	}
+	public WebElement getopenlist() {
+
+		wait = new WebDriverWait (driver,15);
+		wait.until(ExpectedConditions.elementToBeClickable(openlist));
+		return driver.findElement(openlist);
+	}
+
+	public WebElement getunpin() {
+		wait = new WebDriverWait (driver,15);
+		wait.until(ExpectedConditions.elementToBeClickable(unpin));
+		return driver.findElement(unpin);
 	}
 }
