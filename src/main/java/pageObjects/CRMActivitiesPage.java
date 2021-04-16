989772f43e85304df816b7ec848d6cfbb2ec2f5e
nilamma <nilamma@cybage.com>
2021-04-16 19:08:39 +0530

    Adding code for T483,T484,T485,T486

---
+++ src/main/java/pageObjects/CRMActivitiesPage.java
@@ -0,0 +1,193 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CRMActivitiesPage {
+	public WebDriver driver;
+	public FluentWait<WebDriver> wait;
+
+	By myactivitieslabel = By.xpath("//span[contains(text(),'My Activities')]"); //Locator for My Activities header label
+	By appointmentoptninheader = By.xpath("//button[@aria-label='Appointment']"); //Locator for Appointment option in header
+	By newappointmentformtitle = By.xpath("//h1[contains(text(),'New Appointment')]"); //Locator for New Appointment form title
+	By requiredfieldtxtbox = By.xpath("//input[@aria-label='Required, Multiple Selection Lookup']"); //Locator for Required field text box
+	By requiredfieldlabel = By.xpath("//label[contains(text(),'Required')]"); //Locator for Required field label
+	By selectrecordinrequiredfield = By.xpath("//ul[@aria-label='Lookup recently used results']/li[1]"); //Locator to select a record in Required text field
+	By subjecttextbox = By.xpath("//input[@aria-label='Subject']"); //Locator for Subject text box
+	By savenclosebtnonapptform = By.xpath("//button[@aria-label='Save & Close']"); //Locator for Save & Close btn on Appt form
+	By activitytypeddbtn = By.xpath("//div[@id='activityTypeFilterBoxwrapper']/button"); //Locator for Activity Type Drop-down btn
+	By selectapptactivity = By.xpath("//div[@id='activityTypeFilterBox-list']/div[2]"); //Locator for Appointment checkbox
+	By activitytypelabel = 	By.xpath("//span[text()='Activity Type']"); //Locator for Activity Type Label
+	By activitysearchfield = By.xpath("//input[@aria-label='Search this view']"); //Locator for Activity Search field
+	By validateapptname = By.xpath("//div[@data-id='cell-0-3']"); //Locator to validate new appointment name in Search results
+	By openrequiredfieldtext = By.xpath("//div[@data-id='requiredattendees.fieldControl-LookupResultsDropdown_requiredattendees_selected_tag_text']"); //Locator to open the Required field text
+	By taskoptninheader = By.xpath("//button[@aria-label='Task']"); //Locator for Task option in header
+	By newtaskformtitle = By.xpath("//h1[contains(text(),'New Task')]"); //Locator for New Task form title
+	By regardingtextbox = By.xpath("//input[@aria-label='Regarding, Lookup']"); //Locator for Regarding text box
+	By regardingfieldlabel = By.xpath("//label[contains(text(),'Regarding')]"); //Locator for Regarding field label
+	By selectrecordinregardingfield = By.xpath("//ul[@aria-label='Lookup recently used results']/li[2]"); //Locator to select a record in Regarding text field
+	By selecttaskactivity = By.xpath("//div[@id='activityTypeFilterBox-list']/div[20]"); //Locator for Task checkbox
+	By validatetaskname = By.xpath("//div[@data-id='cell-0-3']"); //Locator to validate new task name in Search results
+	By openregardingfieldtext = By.xpath("//div[@data-id='regardingobjectid.fieldControl-LookupResultsDropdown_regardingobjectid_selected_tag_text']"); //Locator to open the Regarding field text
+	By phonecalloptninheader = By.xpath("//button[@aria-label='Phone Call']"); //Locator for Phone Call option in header
+	By newphonecallformtitle = By.xpath("//h1[contains(text(),'New Phone Call')]"); //Locator for New Phone Call form title
+	By calltotextbox = By.xpath("//input[@aria-label='Call To, Multiple Selection Lookup']"); //Locator for Call To Text box
+	By selectrecordincalltofield = By.xpath("//ul[@aria-label='Lookup recently used results']/li[1]"); //Locator to select a record in Call to text field
+	By calltofieldlabel = By.xpath("//label[contains(text(),'Call To')]"); //Locator for Call To field label
+	By selectphonecallactivity = By.xpath("//div[@id='activityTypeFilterBox-list']/div[14]"); //Locator for Phone Call checkbox
+	By validatephonecallname = By.xpath("//div[@data-id='cell-0-3']"); //Locator to validate new phone call name in Search results
+	By emailoptninheader = By.xpath("//button[@aria-label='Email']"); //Locator for Email option in header
+	By newemailformtitle = By.xpath("//h1[contains(text(),'New Email')]"); //Locator for New Email form title
+	By subjectfieldlabel = By.xpath("//label[contains(text(),'Subject')]"); //Locator for Subject field label
+	By emailbody = By.xpath("//body[@class='cke_editable cke_editable_themed cke_contents_ltr']"); //Locator for Email body
+	By selectemailactivity = By.xpath("//div[@id='activityTypeFilterBox-list']/div[9]"); //Locator for Email checkbox
+	By validateemailname = By.xpath("//div[@data-id='cell-0-3']"); //Locator to validate new email name in Search results
+	By fonttext = By.xpath("//span[contains(text(),'Font')]"); //Locator for Font text
+	
+	public CRMActivitiesPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	public WebElement getMyActivitiesLabel() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(myactivitieslabel));
+		return driver.findElement(myactivitieslabel);
+	}
+	public WebElement getAppointmentOptnInHeader() {
+		return driver.findElement(appointmentoptninheader);
+	}
+	public WebElement getNewAppointmentFormTitle() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(newappointmentformtitle));
+		return driver.findElement(newappointmentformtitle);		
+	}
+	public WebElement getRequiredFieldTxtBox() {
+		return driver.findElement(requiredfieldtxtbox);
+	}
+	public WebElement getRequiredFieldLabel() {
+		return driver.findElement(requiredfieldlabel);
+	}
+	public WebElement selectRecordInRequiredfField() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(selectrecordinrequiredfield));
+		return driver.findElement(selectrecordinrequiredfield);		
+	}
+	public WebElement getSubjectTextBox() {
+		return driver.findElement(subjecttextbox);
+	}
+	public WebElement getSavenCloseBtnOnApptForm() throws InterruptedException {
+		Thread.sleep(6000);
+		return driver.findElement(savenclosebtnonapptform);
+	}
+	public WebElement getActivityTypeDDBtn() throws InterruptedException {
+		Thread.sleep(10000);
+		return driver.findElement(activitytypeddbtn);
+	}
+	public WebElement selectApptActivity() throws InterruptedException {
+		Thread.sleep(3000);
+		return driver.findElement(selectapptactivity);
+	}
+	public WebElement getActivityTypeLabel() throws InterruptedException {
+		Thread.sleep(3000);
+		return driver.findElement(activitytypelabel);
+	}
+	public WebElement getActivitySearchField() {
+		return driver.findElement(activitysearchfield);
+	}
+	public WebElement getValidateApptName() throws InterruptedException {
+		Thread.sleep(4000);
+		return driver.findElement(validateapptname);
+	}
+	public WebElement openRequiredFieldText() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(openrequiredfieldtext));
+		return driver.findElement(openrequiredfieldtext);
+	}
+	public WebElement getTaskOptnInHeader() {
+		return driver.findElement(taskoptninheader);
+	}
+	public WebElement getNewTaskFormTitle() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(newtaskformtitle));
+		return driver.findElement(newtaskformtitle);		
+	}
+	public WebElement getRegardingFieldTxtBox() {
+		return driver.findElement(regardingtextbox);
+	}
+	public WebElement getRegardingFieldLabel() {
+		return driver.findElement(regardingfieldlabel);
+	}
+	public WebElement selectRecordInRegardingField() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(selectrecordinregardingfield));
+		return driver.findElement(selectrecordinregardingfield);		
+	}
+	public WebElement selectTaskActivity() throws InterruptedException {
+		Thread.sleep(3000);
+		return driver.findElement(selecttaskactivity);
+	}
+	public WebElement getValidateTaskName() throws InterruptedException {
+		Thread.sleep(4000);
+		return driver.findElement(validatetaskname);
+	}
+	public WebElement openRegardingFieldText() throws InterruptedException {
+		Thread.sleep(5000);
+		return driver.findElement(openregardingfieldtext);
+	}
+	public WebElement getPhoneCallOptnInHeader() {
+		return driver.findElement(phonecalloptninheader);
+	}
+	public WebElement getNewPhoneCallFormTitle() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(newphonecallformtitle));
+		return driver.findElement(newphonecallformtitle);		
+	}
+	public WebElement getCallToFieldTxtBox() {
+		return driver.findElement(calltotextbox);
+	}
+	public WebElement selectRecordInCallToField() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(selectrecordincalltofield));
+		return driver.findElement(selectrecordincalltofield);		
+	}
+	public WebElement getCallToFieldLabel() {
+		return driver.findElement(calltofieldlabel);
+	}
+	public WebElement selectPhoneCallActivity() throws InterruptedException {
+		Thread.sleep(3000);
+		return driver.findElement(selectphonecallactivity);
+	}
+	public WebElement getValidatePhoneCallName() throws InterruptedException {
+		Thread.sleep(4000);
+		return driver.findElement(validatephonecallname);
+	}
+	public WebElement getEmailOptnInHeader() {
+		return driver.findElement(emailoptninheader);
+	}
+	public WebElement getNewEmailFormTitle() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(newemailformtitle));
+		return driver.findElement(newemailformtitle);		
+	}
+	public WebElement getSubjectFieldLabel() {
+		return driver.findElement(subjectfieldlabel);
+	}
+	public WebElement getEmailBody() {
+		return driver.findElement(emailbody);
+	}
+	public WebElement selectEmailActivity() throws InterruptedException {
+		Thread.sleep(3000);
+		return driver.findElement(selectemailactivity);
+	}
+	public WebElement getValidateEmailName() throws InterruptedException {
+		Thread.sleep(4000);
+		return driver.findElement(validateemailname);
+	}
+	public WebElement getFontText() throws InterruptedException {
+		Thread.sleep(4000);
+		return driver.findElement(fonttext);
+	}
+}
---
+++ src/test/java/crmframework/crmAutomation/ActivitiesPageTest.java
@@ -0,0 +1,349 @@
+package crmframework.crmAutomation;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import pageObjects.AppLandingPage;
+import pageObjects.CRMAccountsPage;
+import pageObjects.CRMActivitiesPage;
+import pageObjects.CRMAddMarketingRelationshipOwner;
+import pageObjects.CRMContactPage;
+import pageObjects.CRMHomePage;
+import pageObjects.CRMIncentiveDetailsPage;
+import pageObjects.CRMIncentiveTab;
+import pageObjects.CRMIncentivesPage;
+import pageObjects.CRMLandingPage;
+import pageObjects.CRMListManagementPage;
+import pageObjects.CRMLoginPage;
+import pageObjects.CRMPeoplePage;
+import resources.GenerateData;
+import resources.Utility;
+import resources.base;
+
+@Listeners({TestListeners.class})
+public class ActivitiesPageTest extends base {
+
+	public WebDriverWait wait;
+	public String accnameText;
+	public String phoneno;
+	public GenerateData genData;
+	public String buysatCorplvl, outofbusiness;
+	public String ContactFirstName;
+	public String ContactLastName;
+	CRMLandingPage lap;
+	CRMLoginPage lp;
+	AppLandingPage alp;
+	CRMHomePage hp;
+	CRMAccountsPage ap;
+	CRMIncentiveTab inc;
+	CRMAddMarketingRelationshipOwner amro;
+	Actions act;
+	CRMContactPage cp;
+	CRMPeoplePage pl;
+	Utility utl;
+	CRMIncentivesPage in;
+	CRMIncentiveDetailsPage ind;
+	CRMListManagementPage lmp;
+	CRMActivitiesPage actp;
+	public String listNameText;
+	
+	@BeforeTest
+	public void initialize() throws IOException
+	{
+		driver = initializeDriver();
+		genData=new GenerateData();
+		utl=new Utility(driver);
+	}
+
+	@Test(priority=1)
+	public void TS001_VerifyHomePageTest() throws IOException, InterruptedException {
+
+		//The purpose of this test case to verify:-
+		//TS1- Login to CRM Application and  Select published Apps (Demand Driver Management)
+
+		driver.get(prop.getProperty("url")); //CRM App
+		driver.manage().window().maximize();
+		lap = new CRMLandingPage(driver);
+		lap.getLogin().sendKeys(System.getenv("username"));
+		lap.getnext().click();
+
+		lp= new CRMLoginPage(driver);
+		lp.getpwd().click();
+		lp.getpwd().sendKeys(System.getenv("password"));
+		Thread.sleep(15000);
+		lp.getsignin().click();
+		//Wait to enter the verification code from Mobile
+		Thread.sleep(30000);
+		lp.getVerify().click();
+		lp.getdontshowcheckbox().click();
+		lp.getsigninYes().click();
+		//to wait on Published App Landing page
+		Thread.sleep(15000);
+		driver.switchTo().frame("AppLandingPage");
+		alp = new AppLandingPage(driver);
+		//select Demand Driver application on Landing Page
+		alp.getddm().click();
+
+		hp = new CRMHomePage(driver);
+		hp.getHometitle().isDisplayed();
+		System.out.println("Login to CRM successfully");
+	}
+	
+	@Test(priority=2)
+	public void TS002_VerifyCreateAppointmentTest() throws InterruptedException
+	{
+		//The purpose of this test case to:-
+		//CRM-T485- Verify Creation of Appointment under Activities
+
+		hp = new CRMHomePage(driver);
+		ap = new CRMAccountsPage(driver);
+		actp = new CRMActivitiesPage(driver);
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		//Navigate to Activities under Home in left menu
+		hp.getActivitiesTab().click();
+		
+		//Verify that be default My Activities page is displayed
+		Assert.assertTrue(actp.getMyActivitiesLabel().isDisplayed());
+		
+		//Click on Appointment option from the header
+		actp.getAppointmentOptnInHeader().click();
+		
+		//Verify that New Appointment form page should be displayed
+		Assert.assertTrue(actp.getNewAppointmentFormTitle().isDisplayed());
+		
+		//Enter the required details
+		actp.getRequiredFieldTxtBox().click();
+		actp.selectRecordInRequiredfField().click();
+		actp.getRequiredFieldLabel().click();
+		
+		actp.getSubjectTextBox().click();
+		actp.getSubjectTextBox().sendKeys("CybAppt_" + genData.generateRandomString(3));
+		String apptname = actp.getSubjectTextBox().getAttribute("Value");
+		System.out.println("Newly created appointment name: "+ apptname);
+		
+		//Click on Save&Close button from the header.
+		actp.getSavenCloseBtnOnApptForm().click();
+		
+		//Click on Activity type drop-down btn
+		actp.getActivityTypeDDBtn().click();
+		actp.selectApptActivity().click();
+		actp.getActivityTypeLabel().click();
+		
+		//Search and verify for newly created appointment name
+		actp.getActivitySearchField().click();
+		actp.getActivitySearchField().sendKeys(apptname);
+		hp.getstartsearch().click();
+		
+		Assert.assertTrue(actp.getValidateApptName().getAttribute("title").contains(apptname));
+		
+		//Open the appointment and navigate to the regarding Account/Contact
+		actp.getValidateApptName().click();
+		actp.openRequiredFieldText().click();
+		Thread.sleep(5000);
+		
+		//Verify that appointment name is displayed on Account/Contact page
+		Assert.assertTrue(ap.getverifyaccountppointment().getText().contains(apptname));	
+		ap.getPageBackBtn().click();
+	}
+	
+	@Test(priority=3)
+	public void TS003_VerifyCreateTaskTest() throws InterruptedException
+	{
+		//The purpose of this test case to:-
+		//CRM-T483- Verify Creation of Task under Activities
+
+		hp = new CRMHomePage(driver);
+		ap = new CRMAccountsPage(driver);
+		actp = new CRMActivitiesPage(driver);
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		//Navigate to Activities under Home in left menu
+		hp.getActivitiesTab().click();
+		
+		//Click on Task in the header to create new Task
+		actp.getTaskOptnInHeader().click();
+		
+		//Verify that New Task form should be displayed
+		Assert.assertTrue(actp.getNewTaskFormTitle().isDisplayed());
+		
+		//Enter required data
+		actp.getSubjectTextBox().click();
+		actp.getSubjectTextBox().sendKeys("CybTask_" + genData.generateRandomString(3));
+		String taskname = actp.getSubjectTextBox().getAttribute("Value");
+		System.out.println("Newly created task name: "+ taskname);
+		
+		actp.getRegardingFieldTxtBox().click();
+		actp.selectRecordInRegardingField().click();
+		actp.getRegardingFieldLabel().click();
+		
+		//Click on Save & Close button
+		actp.getSavenCloseBtnOnApptForm().click();
+		
+		//Click on Activity type drop-down btn
+		actp.getActivityTypeDDBtn().click();
+		actp.selectTaskActivity().click();
+		actp.getActivityTypeLabel().click();
+
+		//Search and verify for newly created task name
+		actp.getActivitySearchField().click();
+		actp.getActivitySearchField().sendKeys(taskname);
+		hp.getstartsearch().click();
+		
+		Assert.assertTrue(actp.getValidateTaskName().getAttribute("title").contains(taskname));
+		
+		//Open the appointment and navigate to the regarding Account/Contact
+		actp.getValidateApptName().click();
+		actp.openRegardingFieldText().click();
+		Thread.sleep(5000);
+		utl.scrollToElement(ap.getDescriptionLabel());
+		
+		//Verify that task name is displayed on Account/Contact page
+		Assert.assertTrue(ap.getverifyaccountppointment().getText().contains(taskname));	
+		ap.getPageBackBtn().click();
+	}
+	
+	@Test(priority=4)
+	public void TS004_VerifyCreatePhoneCallTest() throws InterruptedException
+	{
+		//The purpose of this test case to:-
+		//CRM-T486- Verify Creation of Phone Call under Activities
+
+		hp = new CRMHomePage(driver);
+		ap = new CRMAccountsPage(driver);
+		actp = new CRMActivitiesPage(driver);
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		//Navigate to Activities under Home in left menu
+		hp.getActivitiesTab().click();
+		
+		//Click on Phone Call available in the header section
+		actp.getPhoneCallOptnInHeader().click();
+		
+		//Verify that New Phone Call form should be displayed
+		Assert.assertTrue(actp.getNewPhoneCallFormTitle().isDisplayed());
+		
+		//Enter the required details
+		actp.getSubjectTextBox().click();
+		actp.getSubjectTextBox().sendKeys("CybPhoneCall_" +genData.generateRandomString(3));
+		String phonecallname = actp.getSubjectTextBox().getAttribute("Value");
+		System.out.println("Newly created Phone Call name: "+ phonecallname);
+		
+		actp.getCallToFieldTxtBox().click();
+		actp.selectRecordInCallToField().click();
+		actp.getCallToFieldLabel().click();
+		
+		actp.getRegardingFieldTxtBox().click();
+		actp.selectRecordInRegardingField().click();
+		actp.getRegardingFieldLabel().click();
+		
+		//Click on Save & Close button
+		actp.getSavenCloseBtnOnApptForm().click();
+
+		//Click on Activity type drop-down btn
+		actp.getActivityTypeDDBtn().click();
+		actp.selectPhoneCallActivity().click();
+		actp.getActivityTypeLabel().click();
+
+		//Search and verify for newly created Phone Call name
+		actp.getActivitySearchField().click();
+		actp.getActivitySearchField().sendKeys(phonecallname);
+		hp.getstartsearch().click();
+
+		Assert.assertTrue(actp.getValidatePhoneCallName().getAttribute("title").contains(phonecallname));
+		
+		//Open the appointment and navigate to the regarding Account/Contact
+		actp.getValidatePhoneCallName().click();
+		actp.openRegardingFieldText().click();
+		Thread.sleep(5000);
+
+		//Verify that task name is displayed on Account/Contact page
+		Assert.assertTrue(ap.getverifyaccountppointment().getText().contains(phonecallname));	
+		ap.getPageBackBtn().click();
+	}
+	
+	@Test(priority=5)
+	public void TS005_VerifyCreateEmailTest() throws InterruptedException
+	{
+		//The purpose of this test case to:-
+		//CRM-T484- Verify Creation of Email under Activities
+
+		hp = new CRMHomePage(driver);
+		ap = new CRMAccountsPage(driver);
+		actp = new CRMActivitiesPage(driver);
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		//Navigate to Activities under Home in left menu
+		hp.getActivitiesTab().click();
+		
+		//Click on Email available in the header section
+		actp.getEmailOptnInHeader().click();
+		
+		//Verify that New Email form should be displayed		
+		Assert.assertTrue(actp.getNewEmailFormTitle().isDisplayed());
+		
+		//Enter the required details
+		actp.getSubjectTextBox().click();
+		actp.getSubjectTextBox().sendKeys("CybEmail_" +genData.generateRandomString(3));
+		String emailname = actp.getSubjectTextBox().getAttribute("Value");
+		System.out.println("Newly created Email name: "+ emailname);
+			
+		utl.scrollToElement(actp.getSubjectFieldLabel());
+		Thread.sleep(4000);
+		actp.getRegardingFieldTxtBox().click();
+		actp.selectRecordInRegardingField().click();
+		actp.getRegardingFieldLabel().click();
+
+		//Click on Save & Close button
+		actp.getSavenCloseBtnOnApptForm().click();
+		
+		//Click on Activity type drop-down btn
+		actp.getActivityTypeDDBtn().click();
+		actp.selectEmailActivity().click();
+		actp.getActivityTypeLabel().click();
+
+		//Search and verify for newly created Email name
+		actp.getActivitySearchField().click();
+		actp.getActivitySearchField().sendKeys(emailname);
+		hp.getstartsearch().click();
+		
+		Assert.assertTrue(actp.getValidateEmailName().getAttribute("title").contains(emailname));
+		
+		//Open the appointment and navigate to the regarding Account/Contact
+		actp.getValidateEmailName().click();
+		actp.getSubjectFieldLabel().click();
+		
+		/*act = new Actions(driver);
+		act.keyDown(Keys.CONTROL).sendKeys(Keys.END).perform();
+		Thread.sleep(5000);
+		
+		actp.openRegardingFieldText().click();
+		actp.getRegardingFieldLabel().click();
+		act.click(actp.openRegardingFieldText()).perform();
+		actp.openRegardingFieldText().click();
+		//actp.openRegardingFieldText().click();
+		Thread.sleep(5000);
+
+		//Verify that task name is displayed on Account/Contact page
+		Assert.assertTrue(ap.getverifyaccountppointment().getText().contains(emailname));	*/
+		ap.getPageBackBtn().click();
+	}
+
+}
