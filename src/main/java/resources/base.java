d5d4e59cc0078a00bcc7c7686d0884ab7101d02f
nilamma <nilamma@cybage.com>
2021-07-28 10:21:32 +0530

    Pushing code to take screenshots on Test Failure

--- src/main/java/resources/base.java
+++ src/main/java/resources/base.java
@@ -1,21 +1,26 @@
 package resources;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class base {	
-	
+
 	// this is base class page where driver is initialized and data.properties mentioned. 
 	// this class holds all the functions that commonly used by other pages.
 	public static WebDriver driver;
 	public static Properties prop;
-	
+
 	public WebDriver initializeDriver() throws IOException
 	{
 		prop= new Properties();
@@ -25,7 +30,7 @@ public class base {
 		prop.load(fls);
 		String browserName=prop.getProperty("browser");
 		System.out.println(browserName);
-		
+
 		if(browserName.equals("chrome"))
 		{
 			// you can mention chromedriver.exe path here to execute all the scripts.
@@ -46,9 +51,19 @@ public class base {
 			driver = new InternetExplorerDriver();
 			driver.manage().window().maximize();
 		}
-		
+
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
-		 
+
 	}	
+
+	public static String capture(WebDriver driver, String testMethodName) throws IOException 
+	{
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String screenShotName = testMethodName + ".png";
+		File Dest = new File("src/../screenshots/" + screenShotName);
+		String errflpath = Dest.getAbsolutePath();
+		FileUtils.copyFile(scrFile, Dest);
+		return errflpath;
+	}
 }
